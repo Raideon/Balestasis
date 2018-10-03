40e7fe78bf947f3c6673d0e41f2b4606df9ee46d
WrithingNova <luoliangyi@gmail.com>
2018-10-03 14:52:58 +0900

    Update raw_to_net.sh

--- raw_to_net.sh
+++ raw_to_net.sh
@@ -15,29 +15,29 @@ fi
 
 
 #put the data file in the data folder, the extension should be .dat
-#data file should be in the format as follows: 
+#data file should be in the format shown as follows: 
 #	the first line is the number of variables, 
 #	the second line is the number of data points/instances, 
 #	the third line is variable names, 
 #	the fourth line is variables' #state, 
 #	then instances line by line.
 
-#set the name of the data file without the extension and change the extension of the file to .dat, for example, data from data.dat
+#put here the name of the data file without the extension and change the extension of the file to .dat, for example, data from data.dat
 datafile=<filename>
 
 #set the time (per node) for cache construction (searching local strutures/parent sets). 
-#This time limit isn't strict. For ordinarily complex networks with less than 1000 nodes, 5-10 seconds is usually enough. 
+#This time limit is not absolute. For ordinarily complex networks with less than 1000 nodes, 5-10 seconds is usually enough. 
 psiTime=5
 
 #set the time for building global structures. 
-#Only the network found within the limited will be recorded or saved, which might not be necessary outside experimental usages. 
+#Only the network found within the limited will be recorded or saved.
 soTime=2
 
-#set the number of ASOBS restarts. The total ASOBS running time will be restart*soTime. Sometimes, for example, it is better to run five 2-second runs than a single 10-second run.
+#set the number of ASOBS restarts. The total ASOBS running time will be restart*soTime. (Sometimes, for example, it is better to run five 2-second runs than a single 10-second run.)
 restart=1
 
-#if you want to save the learnt network to a file, set it as 1, otherwise 0. (The log will always say the network has been saved.)
-#Saved network will be placed in the network folder.
+#if you want to save the learnt network to a file, set it to 1, otherwise 0. (The log will always say that the networks are saved.)
+#Networks will be saved in the network folder.
 output=0
 
 
